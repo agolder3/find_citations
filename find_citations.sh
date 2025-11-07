630b936d481d5823bfc79955bbdb05be1076203b
agolder3 <105230670+agolder3@users.noreply.github.com>
2025-11-07 13:11:29 -0800

    Update find_citations.sh

--- find_citations.sh
+++ find_citations.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export DOI=$1 # put your input DOI here (not as a URL, just the DOI itself)
 export FILENAME=$2
-curl -X GET "https://opencitations.net/index/api/v2/citations/doi:$DOI" | jq --raw-output ".[]|.citing" | doi2bib -i - -o "${FILENAME}-citations.bib"
+curl -X GET "https://api.opencitations.net/index/v2/citations/doi:$DOI" | jq --raw-output ".[]|.citing" | doi2bib -i - -o "${FILENAME}-citations.bib"
