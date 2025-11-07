bc931f7e2e2bea3d8e014266758c9b5f736503f9
agolder3 <105230670+agolder3@users.noreply.github.com>
2025-11-07 13:12:05 -0800

    Update generate_bib.py

--- generate_bib.py
+++ generate_bib.py
@@ -4,4 +4,4 @@ f     = open("publication_list.txt", "r")
 lines = f.readlines()
 
 for line in lines:
-    os.system("./find_citations.sh " + line.strip("\n").split(" , ")[1] + " " + line.strip("\n").split(" , ")[0].replace(" ", "_").replace(":", "").replace(">", "").replace("Ω", "ohm"))
+    os.system("source find_citations.sh " + line.strip("\n").split(" , ")[1] + " " + line.strip("\n").split(" , ")[0].replace(" ", "_").replace(":", "").replace(">", "").replace("Ω", "ohm"))
